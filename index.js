9297afe4b1b1630a05a8b1a7bd9f40c660eec2a6
bell0x <142863201+oveuabe@users.noreply.github.com>
2025-08-02 22:04:19 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-4">
-      <h1 className="text-2xl font-bold">UCH Scanner</h1>
-      <p className="mt-2">Gunakan menu "Scanner" untuk mulai memindai inventaris.</p>
-    </main>
-  );
-}
